27f6ea91ebdf95b3d4195031c1c28c5d865edb00
Benjamín <benjamin.diaze@outlook.com>
2025-10-26 22:09:14 -0300

    Se añade spartan/ui

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HlmButtonImports } from '@spartan-ng/helm/button';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
+    selector: 'app-root',
+    imports: [RouterOutlet, HlmButtonImports],
+    templateUrl: './app.html',
+    styleUrl: './app.scss',
 })
 export class App {
-  protected readonly title = signal('ankatu-landing-page');
+    protected readonly title = signal('ankatu-landing-page');
 }
